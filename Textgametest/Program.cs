28b535bcc8de80ad1244081886e73086c90ce6bc
Something226 <axel.lieback@gmail.com>
2020-12-10 09:54:10 +0100

    Commit1

---
+++ Textgametest/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Textgametest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
