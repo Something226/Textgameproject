a18bc483062a43a639f768508a43353cb02ae86e
Something226 <axel.lieback@gmail.com>
2021-01-07 16:31:26 +0100

    Basecommit

--- Textgametest/Program.cs
+++ Textgametest/Program.cs
@@ -1,13 +1,27 @@
 ﻿using System;
 
-namespace Textgametest
+namespace Textgametest 
 {
     class Program
     {
         static void Main(string[] args)
         {
-           t
+            //Variables are stated here
+            //Player stats are determined by character customisation
+            Random generator = new Random();
 
+            string userinput;
+
+            int playerhp;
+            int playerdmg;
+            int playerdodge;
+
+            int enemyhp;
+            int enemydmg; 
+            int enemydodge;
+
+            System.Console.WriteLine("Welcome to my text based game!\nFirst, you'll have to create a character");
+            
 
 
             Console.ReadLine();
